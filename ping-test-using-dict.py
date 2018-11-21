a937d51ce6998a18e5e8bcd63f7a04039a736516
SahanaJSJ <sjayaramu@eplus.com>
2018-11-22 01:10:59 +0530

    Added Platform checking

--- ping-test-using-dict.py
+++ ping-test-using-dict.py
@@ -7,14 +7,22 @@ servers = {"10.1.185.71", "10.1.185.66", "10.1.185.89"}
 
 for vm in servers:
     print "---- Trying to Ping a Server with IPAddress ----", vm
-    response = os.system("ping -c 1 " + vm)
+    # Check for Windows and Linux Platforms
+    if plat == "Windows":
+        response = os.system("ping -n 1 " + vm)
+        pass
+
+    elif plat == "Linux":
+        response = os.system("ping -c 1 -W 3 " + vm)
+        pass
+
     #and then check the response...
     if response == 0:
         print "***********************************"
         print(vm, 'is UP!')
         print "***********************************"
         print "\n"
-    elif response == 2:
+    elif response == 2 or 256 or 512:
         print "***********************************"
         print(vm, 'is DOWN and No response from Server!')
         print "***********************************"
